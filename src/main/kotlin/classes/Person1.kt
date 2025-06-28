b4553cb8e62762c26b4ac5ea85efb074269aaa3b
tyeerth <tyeerth@163.com>
2025-06-28 15:06:44 +0800

    1

--- src/main/kotlin/classes/Person1.kt
+++ src/main/kotlin/classes/Person1.kt
@@ -16,6 +16,7 @@ class Person(
 fun main() {
     val person = Person("Alice", "Smith", 30)
     println(person.firstName)
+    person.name
     val person1 = Person()
     println(person1.age)
     println(person.lastName)
